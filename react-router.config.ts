cb74a79b46ba43866a8b07f81c1bc68b6be5440f
tomokijs <tomoki@simplesky.org>
2025-03-29 12:31:49 +0900

    vercel setting

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,2 +1,3 @@
-import type { Config } from "@react-router/dev/config"
-export default { ssr: true } satisfies Config
+import type { Config } from '@react-router/dev/config'
+import { vercelPreset } from '@vercel/react-router/vite'
+export default { ssr: true, presets: [vercelPreset()] } satisfies Config
